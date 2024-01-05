4adb2a7f4e01c45ae7b9455084d28d906653a982
许佳豪 <10351874+xujiahao29@user.noreply.gitee.com>
2024-01-05 12:43:51 +0800

    Site updated: 2024-01-05 12:43:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/11/24/几种卡片校验算法/","2023/11/20/ChatGPT使用教程/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2024/01/05/GPT4免费使用教程/","2023/11/20/ChatGPT使用教程/","2023/11/24/几种卡片校验算法/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
