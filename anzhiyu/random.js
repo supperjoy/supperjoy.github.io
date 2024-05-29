8f9964e7d5d36c0547b24ffd6ab336b60fb22577
许佳豪 <10351874+xujiahao29@user.noreply.gitee.com>
2024-05-29 17:27:48 +0800

    Site updated: 2024-05-29 17:27:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/11/24/几种卡片校验算法/","2024/05/16/FLIP动画实现拖拽排序/","2024/01/07/实现material文本框效果/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2024/05/16/FLIP动画实现拖拽排序/","2024/05/29/3D轮播图实现/","2024/01/07/实现material文本框效果/","2023/11/24/几种卡片校验算法/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
