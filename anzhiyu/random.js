081dd2134a508aa748d7a48f5b9c483ac3abfa1d
许佳豪 <10351874+xujiahao29@user.noreply.gitee.com>
2024-08-30 12:03:48 +0800

    Site updated: 2024-08-30 12:03:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2024/05/29/3D轮播图实现/","2024/05/16/FLIP动画实现拖拽排序/","2024/05/30/py文件加密/","2023/11/24/几种卡片校验算法/","2024/01/07/实现material文本框效果/","2024/08/30/vue快速上手/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2024/05/29/3D轮播图实现/","2024/05/30/py文件加密/","2024/05/16/FLIP动画实现拖拽排序/","2023/11/24/几种卡片校验算法/","2024/01/07/实现material文本框效果/","2024/08/30/vue快速上手/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
